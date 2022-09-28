aacee468a77ee3c8d05f86124a5576dd021a798f
Alexander Revo <alrevo@gmail.com>
2022-09-28 20:27:49 +0200

    Add doc comments and extra error handling.

--- flatten-sdcard-album.py
+++ flatten-sdcard-album.py
@@ -1,3 +1,8 @@
+"""
+Will move all the files from their respective subfolders into the `Album` folder
+and rename them in `YYYY-MM-DD_HH-mm-SS` format, keeping their original extensions.
+"""
+
 import os
 import re
 import sys
@@ -6,19 +11,33 @@ import sys
 pattern = re.compile(r'(.{4})(.{2})(.{2})(.{2})(.{2})(.{2}).*\.(.{3})')
 
 
-def flatten_and_rename(current_dir):
+def flatten_and_rename(current_dir, target_dir):
+    """Traverse directories recursively and move matching files to `target_dir`."""
     for item in os.listdir(current_dir):
         current_item = os.path.join(current_dir, item)
         if os.path.isdir(current_item):
-            flatten_and_rename(current_item)
+            flatten_and_rename(current_item, target_dir)
         else:
-            m = pattern.match(item)
-            # TODO: make sure to not overwrite if two files were made in the same second
+            match = pattern.match(item)
+            if not match:
+                print(f'Skipping "{current_item}": unexpected name format.')
+                continue
+
             new_name = os.path.join(
-                working_dir,
-                (f'{m.group(1)}-{m.group(2)}-{m.group(3)}_'
-                 f'{m.group(4)}-{m.group(5)}-{m.group(6)}.{m.group(7)}')
+                target_dir,
+                (f'{match.group(1)}-{match.group(2)}-{match.group(3)}_'
+                 f'{match.group(4)}-{match.group(5)}-{match.group(6)}.{match.group(7)}')
             )
+
+            i = 0
+            while os.path.exists(new_name):
+                i += 1
+                new_name = os.path.join(
+                    target_dir,
+                    (f'{match.group(1)}-{match.group(2)}-{match.group(3)}_'
+                     f'{match.group(4)}-{match.group(5)}-{match.group(6)}-{i}.{match.group(7)}')
+                )
+
             os.rename(current_item, new_name)
             print(f'{current_item} -> {new_name}')
 
@@ -26,4 +45,4 @@ def flatten_and_rename(current_dir):
 if __name__ == "__main__":
     working_dir = (sys.argv[1] if len(sys.argv) > 1 else '.')
     print(f'Current working dir is "{working_dir}"')
-    flatten_and_rename(working_dir)
+    flatten_and_rename(working_dir, working_dir)
--- rename-files-from-usb.py
+++ rename-files-from-usb.py
@@ -1,3 +1,8 @@
+"""
+Will rename the files in their respective subfolders
+into `YYYY-MM-DD_HH-mm-SS<suffix>` format, keeping their original extensions.
+"""
+
 import os
 import re
 import sys
@@ -7,25 +12,30 @@ pattern = re.compile(r'(.{4})(.{2})(.{2})(.{2})(.{2})(.{2})(.*)\.(.{3})')
 
 
 def rename_usb_album(current_dir):
+    """Traverse subdirectories and rename mathing files."""
     for item in os.listdir(current_dir):
         item_path = os.path.join(current_dir, item)
         if os.path.isdir(item_path):
             for file_name in os.listdir(item_path):
                 file_path = os.path.join(item_path, file_name)
-                m = pattern.match(file_name)
-                if m:
-                    new_name = os.path.join(
-                        item_path,
-                        (f'{m.group(1)}-{m.group(2)}-{m.group(3)}_'
-                        f'{m.group(4)}-{m.group(5)}-{m.group(6)}_{m.group(7)}'
-                        f'.{m.group(8)}')
-                    )
-                    os.rename(file_path, new_name)
-                    print(f'{file_path} -> {new_name}')
-                else:
+
+                match = pattern.match(file_name)
+                if not match:
                     print(f'Skipping "{file_path}": unexpected name format.')
+                    continue
+
+                new_name = os.path.join(
+                    item_path,
+                    (f'{match.group(1)}-{match.group(2)}-{match.group(3)}_'
+                     f'{match.group(4)}-{match.group(5)}-{match.group(6)}_{match.group(7)}'
+                     f'.{match.group(8)}')
+                )
+
+                os.rename(file_path, new_name)
+                print(f'{file_path} -> {new_name}')
         else:
             print(f'Skipping "{item_path}": not a directory.')
+
 
 if __name__ == "__main__":
     working_dir = (sys.argv[1] if len(sys.argv) > 1 else '.')
